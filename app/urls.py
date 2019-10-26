a481e9b188c5da6a168f7a2a05bc4a1729d38247
Vu The Huy <vuthehuyht@gmail.com>
2019-10-26 21:56:56 +0700

    add login function

--- app/urls.py
+++ app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('blog/', views.blog, name='blog'),
-    path('category/', views.shop_category, name='category')
+    path('category/', views.shop_category, name='category'),
+    path('login/', views.login, name='login')
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
 
 
 # Create your views here.
@@ -12,3 +15,28 @@ def blog(request):
 
 def shop_category(request):
     return render(request, 'pages/category.html')
+
+
+def login(request):
+    if request.method == "POST":
+        email = request.POST.get('username').strip()
+        password = request.POST.get('password').strip()
+        if '@' in email:
+            username = User.objects.get(email=email).username
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                auth_login(request, user)
+                return render(request, 'index.html')
+            else:
+                messages.error(request, "Tài khoản hoặc mật khẩu không đúng")
+                return render(request, 'pages/login.html')
+        else:
+            user = authenticate(username=email, password=password)
+            print(user.username)
+            if user is not None:
+                auth_login(request, user)
+                return render(request, 'index.html')
+            else:
+                messages.error(request, "Tài khoản hoặc mật khẩu không đúng")
+                return render(request, 'pages/login.html')
+    return render(request, 'pages/login.html')
