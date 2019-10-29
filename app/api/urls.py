b5d4394f47ae46d8fe61acd373bb57b560c2036a
Vu The Huy <vuthehuyht@gmail.com>
2019-10-29 12:21:33 +0700

    create logout and api register

--- app/api/urls.py
+++ app/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/v1/login', views.login)
+    path('api/v1/login', views.login),
+    path('api/v1/register', views.register)
 ]
